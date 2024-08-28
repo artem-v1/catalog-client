9ccd1925c0b7c91d4f49715c80071f2f707961fa
Artem <artemvovk7777777@gmail.com>
2024-08-28 23:24:12 +0200

    create custom-theme, new pages and components

---
+++ src/app/components/characteristics/characteristics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characteristics',
+  standalone: true,
+  imports: [],
+  templateUrl: './characteristics.component.html',
+  styleUrl: './characteristics.component.scss'
+})
+export class CharacteristicsComponent {
+
+}
--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { OfferCardComponent } from '../../components/offer-card/offer-card.component';
+import { CharacteristicsComponent } from '../../components/characteristics/characteristics.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [OfferCardComponent, CharacteristicsComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
